b5fec8ae9c09ede9dbc7379780db977a9f561907
Sunanda123-png <sunanda02@gmail.com>
2021-09-22 20:59:13 +0530

    Checking player play or not and if play what he get snake or ladder

--- src/com/sunanda/Snakeladder.java
+++ src/com/sunanda/Snakeladder.java
@@ -8,5 +8,20 @@ public class Snakeladder {
         int position=0;
         int randomCheck=(int) Math.floor(Math.random()+1)%6;
         System.out.println("Random Dice number is :- "+randomCheck);
+        int place=(int) Math.floor(Math.random())%3;
+        System.out.println("Place is :- "+place);
+        if(place==1)
+        {
+            System.out.println("Ladder moves ahead by the position receive in the die");
+        }
+        if(place==2)
+        {
+            System.out.println("snake moves behind by the position receive in the die");
+        }
+        else
+        {
+            System.out.println("Incase no playe stay in same position");
+        }
+
     }
 }
