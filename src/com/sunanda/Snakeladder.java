b35e33206dada6865f371ba1165339bf67f93b63
Sunanda123-png <sunanda02@gmail.com>
2021-09-22 21:14:54 +0530

    Repeat till the player reaches the winning position 100

--- src/com/sunanda/Snakeladder.java
+++ src/com/sunanda/Snakeladder.java
@@ -5,23 +5,30 @@ public class Snakeladder {
     {
         System.out.println("Welcome to snake ladder game");
         int player=1;
-        int position=0;
-        int randomCheck=(int) Math.floor(Math.random()+1)%6;
-        System.out.println("Random Dice number is :- "+randomCheck);
-        int place=(int) Math.floor(Math.random())%3;
-        System.out.println("Place is :- "+place);
-        if(place==1)
-        {
-            System.out.println("Ladder moves ahead by the position receive in the die");
+        int position;
+        int position1=0;
+        while (position1<100) {
+            int randomCheck = (int) Math.floor(Math.random() + 1) % 6;
+            System.out.println("Random Dice number is :- " + randomCheck);
+            int place = (int) Math.floor(Math.random()) % 3;
+            System.out.println("Place is :- " + place);
+            if (place == 1)
+            {
+                position1=position1+randomCheck;
+            }
+            if (place == 2)
+            {
+               position1=position1-randomCheck;
+               if(position1<0)
+               {
+                   position1=0;
+               }
+            }
+            else
+            {
+                position1=position1+0;
+            }
+            System.out.println("Player win with the position"+position1);
         }
-        if(place==2)
-        {
-            System.out.println("snake moves behind by the position receive in the die");
-        }
-        else
-        {
-            System.out.println("Incase no playe stay in same position");
-        }
-
     }
 }
