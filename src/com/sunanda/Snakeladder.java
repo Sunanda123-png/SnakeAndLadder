fa0ce0f1f39a681c5aac08eae5863a52d6ea8fa4
Sunanda123-png <sunanda02@gmail.com>
2021-09-22 20:21:48 +0530

    Adding single player at zero position

---
+++ src/com/sunanda/Snakeladder.java
@@ -0,0 +1,10 @@
+package com.sunanda;
+
+public class Snakeladder {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to snake ladder game");
+        int player=1;
+        int position=0;
+    }
+}
